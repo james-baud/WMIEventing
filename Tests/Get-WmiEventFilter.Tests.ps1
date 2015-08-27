c9bf532bd0d5bb99388405caed24857938229db1
Jared Atkinson <jared@invoke-ir.com>
2015-08-27 10:58:32 -0400

    Hopefully fixed the tests...

--- Tests/Get-WmiEventFilter.Tests.ps1
+++ Tests/Get-WmiEventFilter.Tests.ps1
@@ -1,12 +1,24 @@
-﻿Import-Module -Force Uproot
+﻿Import-Module -Force $PSScriptRoot\..\WMIEventing.psd1
 
-Describe 'Get-WmiEventFilter' {    
+Describe 'WMIEventing' {    
     
     Context 'Strict mode' { 
-        #Set-StrictMode -Version latest
         
+        It 'Should add a local Event Filter' {
+            { Add-WmiEventFilter -Name test -Query 'SELECT * FROM Win32_ProcessStartTrace' } | Should Not Throw
+        }
+
         It 'Should work locally' {
             Get-WmiEventFilter | Should Not Be $Null
+        }
+
+        It 'Should work locally by name' {
+            Get-WmiEventFilter -Name test | Should Not Be $Null
+        }
+
+        It 'Should remove the Filter' {
+            Remove-WmiEventFilter
+            Get-WmiEventFilter | Should Be $Null
         }
     }
 }
