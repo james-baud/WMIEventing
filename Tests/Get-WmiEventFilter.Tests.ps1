00dc208e1913f669f6b00514bdc0e7840609aef9
Jared Atkinson <jared@invoke-ir.com>
2015-08-27 10:45:05 -0400

    Added initial unit tests with Pestor and AppVeyor

---
+++ Tests/Get-WmiEventFilter.Tests.ps1
@@ -0,0 +1,12 @@
+﻿Import-Module -Force Uproot
+
+Describe 'Get-WmiEventFilter' {    
+    
+    Context 'Strict mode' { 
+        #Set-StrictMode -Version latest
+        
+        It 'Should work locally' {
+            Get-WmiEventFilter | Should Not Be $Null
+        }
+    }
+}
